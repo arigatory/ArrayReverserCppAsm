e62a1edad5e9cca09e255981df67c99bcf76241b
Ivan MSI <arigatory@gmail.com>
2021-04-01 05:01:29 +0300

    calling asm from c++ file

--- ArrayReverserCppAsm/ArrayReverserCppAsm.cpp
+++ ArrayReverserCppAsm/ArrayReverserCppAsm.cpp
@@ -1,8 +1,25 @@
 
 #include <iostream>
 
+extern "C" void Reverser(int* y, const int* x, int n);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int n = 10;
+    int x[n], y[n];
+    for (int i = 0; i < n; i++)
+    {
+        x[i] = i;
+    }
+
+    Reverser(y, x, n);
+
+    printf("\nArray Reverser-----------------------\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("y: %5d     x: %d\n", y[i], x[i]);
+    }
+
+    return 0;
 }
 
